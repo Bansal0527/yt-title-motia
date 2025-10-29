a29f4f8c7af47eaac9838df8ba11c3ab60b5fbb2
Bansal0527 <mayank.bansal.0527@gmail.com>
2025-10-30 00:09:30 +0530

    types

--- types.d.ts
+++ types.d.ts
@@ -12,7 +12,10 @@ declare module 'motia' {
   }
 
   interface Handlers {
-    'ResolveChannel': EventHandler<never, { topic: 'yt.channel.resolved'; data: never }>
+    'SendEmail': EventHandler<never, never>
+    'GenerateTitles': EventHandler<never, { topic: 'yt.titles.ready'; data: never }>
+    'FetchedVideos': EventHandler<never, { topic: 'yt.videos.fetched'; data: never } | { topic: 'yt.videos.error'; data: never }>
+    'ResolveChannel': EventHandler<never, { topic: 'yt.channel.resolved'; data: never } | { topic: 'yt.channel.error'; data: never }>
     'SubmitChannel': ApiRouteHandler<Record<string, unknown>, unknown, { topic: 'yt.submit'; data: never }>
   }
 }
